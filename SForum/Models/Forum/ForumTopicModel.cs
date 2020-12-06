be232350e4d609b977ee2f2fb09aea11acfc2d3b
Roman <carapacik@gmail.com>
2020-12-06 13:32:32 +0300

    Forum topic view

--- SForum/Models/Forum/ForumTopicModel.cs
+++ SForum/Models/Forum/ForumTopicModel.cs
@@ -7,5 +7,6 @@ namespace SForum.Models.Forum
     {
         public ForumListingModel Forum { get; set; }
         public IEnumerable<PostListingModel> Posts { get; set; }
+        public string SearchQuery { get; set; }
     }
 }
